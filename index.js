702b72c22975ddf289e99c3002cbe689fe037969
Shivam Sharma <sharmashushant118@gmail.com>
2022-10-10 21:03:23 +0530

    Data Receive Complete

---
+++ index.js
@@ -0,0 +1,59 @@
+const cors = require("cors");
+const dotenv = require("dotenv");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+
+dotenv.config({ path: "./config.env" });
+const Intern = require("./model");
+
+const port = process.env.PORT;
+
+require("./connection");
+
+app.get("/", async (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/API", async function (req, res) {
+  const DataF = await Intern.find();
+  res.json(DataF);
+});
+
+app.get("/data", (req, res, next) => {
+  Intern.find()
+    .then((result) => {
+      res.status(200).json({ Data: result });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
+});
+
+// app.post("/API", async function (req, res) {
+//   const { intensity, impact, url, sector, topic } = req.body;
+//   try {
+//     const save_Data = new Intern({
+//       intensity,
+//       impact,
+//       url,
+//       sector,
+//       topic,
+//     });
+//     const DataSaved = await save_Data.save();
+//     if (DataSaved) {
+//       res.status(201).json({ message: "Data Saved Successfully" });
+//     } else {
+//       res.status(500).json({ error: "Failed" });
+//     }
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ model.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+const Schema = new mongoose.Schema({
+  end_year: {
+    type: String,
+    required: false,
+  },
+  intensity: {
+    type: Number,
+    required: false,
+  },
+  sector: {
+    type: String,
+    required: false,
+  },
+  topic: {
+    type: String,
+    required: false,
+  },
+  insight: {
+    type: String,
+    required: false,
+  },
+  url: {
+    type: String,
+    required: false,
+  },
+  start_year: {
+    type: String,
+    required: false,
+  },
+  impact: {
+    type: String,
+    required: false,
+  },
+  added: {
+    type: String,
+    required: false,
+  },
+  published: {
+    type: String,
+    required: false,
+  },
+  country: {
+    type: String,
+    required: false,
+  },
+  relevance: {
+    type: Number,
+    required: false,
+  },
+  pestle: {
+    type: String,
+    required: false,
+  },
+  source: {
+    type: String,
+    required: false,
+  },
+  title: {
+    type: String,
+    required: false,
+  },
+  likelihood: {
+    type: Number,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model("Intern", Schema);;
